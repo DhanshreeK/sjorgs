ee12a708972e593b7de77426c265fe25bba9b065
komya-21 <komalmane21@gmail.com>
2018-05-16 18:38:27 +0530

    some part of import

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+    require 'roo'
 	after_create :send_message
 
     validates :name , presence:true
@@ -29,6 +30,14 @@ class Customer < ApplicationRecord
   
   
 
+def self.to_csv(options = {})
+   CSV.generate(options) do |csv|
+     csv << column_names
+     all.each do |invoice|
+       csv << invoice.attributes.values_at(*column_names)
+     end
+   end
+ end
 #def self.import(file)
  # CSV.foreach(file.path, headers: true) do |row|
   #  Customer.create! row.to_hash
@@ -42,33 +51,19 @@ def self.import(file)
   (2..spreadsheet.last_row).each do |i|
     row = Hash[[header, spreadsheet.row(i)].transpose]
     customer = find_by_id(row["id"]) || new
-    customer.attributes = row.to_hash.slice(*accessible_attributes)
+    customer.attributes = row.to_hash
     customer.save!
   end
+end
 
 
 
 def self.open_spreadsheet(file)
   case File.extname(file.original_filename)
-  when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
-  when '.xls' then Roo::Roo::Excel.new(file.path, nil, :ignore)
-  when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+  when '.csv' then Roo::Csv.new(file.path,packed: false,file_warning: :ignore)
+  when '.xls' then Roo::Excel.new(file.path,packed: false,file_warning: :ignore)
+  when '.xlsx' then Roo::Excelx.new(file.path,packed: false,file_warning: :ignore)
   else raise "Unknown file type: #{file.original_filename}"
   end
 end
-
-
-end
-def self.to_csv(options = {})
-   CSV.generate(options) do |csv|
-     csv << column_names
-     all.each do |invoice|
-       csv << invoice.attributes.values_at(*column_names)
-     end
-   end
- end
-
-
-
-	
 end
